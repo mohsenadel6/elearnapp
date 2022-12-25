714ca7e6f8436444a3482bdcbc3290dc55de72d9
mohsenadel6@gmail.com <mohsenadel6@gmail.com>
2022-12-25 21:30:39 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/elearnapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.elearnapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Courses.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Courses extends StatefulWidget {
+  const Courses({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CoursesState createState() => _CoursesState();
+}
+
+class _CoursesState extends State<Courses> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
